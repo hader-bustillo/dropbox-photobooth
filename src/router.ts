37d841f2f3901737236e05e27ade6d811e6a394b
Hader Bustillo <haderbustillo@gmail.com>
2023-05-09 21:21:05 +0300

    Add new features + refactoring

--- src/router.ts
+++ src/router.ts
@@ -1,41 +1,21 @@
 import express, { Request, Response } from 'express'
 import * as dotenv from 'dotenv'
-import dropboxV2Api from 'dropbox-v2-api'
 import store from './store'
 import fs from 'fs'
-import fsPromises from "node:fs/promises"
-import path from 'node:path'
+
 import { authMiddleware } from './middleware/authMiddleware'
 
-import { Dropbox, DropboxAuth } from 'dropbox'
-import { syncImagesService } from './service/image'
+import { DropboxAuth } from 'dropbox'
+import { resetImagesService, syncImagesService } from './service/image'
 
 dotenv.config()
 
 const router = express.Router()
 
-const dropbox = dropboxV2Api.authenticate({
-  client_id: process.env.DROPBOX_CLIENT_ID,
-  client_secret: process.env.DROPBOX_CLIENT_SECRET,
-  redirect_uri: process.env.DROPBOX_REDIRECT_URI,
-  token_access_type: 'offline', // if you need an offline long-living refresh token
-  state: 'OPTIONAL_STATE_VALUE'
-})
-
-// const dbx = new Dropbox({
-//   clientId: process.env.CLIENT_ID
-// })
-
 const dbxAuth = new DropboxAuth({
   clientId: process.env.DROPBOX_CLIENT_ID,
   clientSecret: process.env.DROPBOX_CLIENT_SECRET,
 })
-
-// console.log('dbxAuth',  dbxAuth)
-
-// router.get('/', (_req: Request, res: Response) => {
-//   return res.send('<a href="/login">Login</html>')
-// })
 
 router.get('/login', async (_req: Request, res: Response) => {
   try {
@@ -55,11 +35,12 @@ router.get('/login', async (_req: Request, res: Response) => {
 })
 
 router.get('/refresh-token', (req, res) => {
-  dropbox.refreshToken(store.refreshToken, (err: any, result: { access_token: string }) => {
-    store.accessToken = result.access_token
-    console.log('refresh token result', result)
+  // TODO: Make refresh token
+  // dropbox.refreshToken(store.refreshToken, (err: any, result: { access_token: string }) => {
+  //   store.accessToken = result.access_token
+  //   console.log('refresh token result', result)
 
-  })
+  // })
 })
 
 router.get('/auth',  async (req, res) => {
@@ -101,32 +82,23 @@ router.get('/auth',  async (req, res) => {
   
 })
 
-router.get('/reset', async () => {
-  store.accessToken = null
-  store.refreshToken = null
+router.get('/reset', resetImagesService)
 
-  const directory = "images";
-
-  for (const file of await fsPromises.readdir(directory)) {
-    await fsPromises.unlink(path.join(directory, file));
-  }
-})
-
-router.get('/get-tokens', (req, res) => {
-  res.send(
+router.get('/get-tokens', (_req: Request, res: Response) => {
+  return res.send(
     'Access token is ' + store.accessToken + 
     '. Refresh token is ' + store.refreshToken
   )
 })
 
-function getImages (req, res) {
+function getImages (_req: Request, res: Response) {
   const results = []
 
   const dir = './images/'
 
   try {
     if (!fs.existsSync(dir)){
-      fs.mkdirSync(dir);
+      fs.mkdirSync(dir)
     }
 
     const files = fs.readdirSync(dir)
@@ -143,99 +115,9 @@ function getImages (req, res) {
 
 router.get('/get-images', getImages)
 
-async function syncImages (req, res, next) {
+async function syncImages (_req: Request, res: Response) {
   await syncImagesService()
   return res.json({ "message": "done" })
-
-  const dbx = new Dropbox({ accessToken: store.accessToken })
-
-  const result = await dbx.filesListFolder({ path: '/samples' })
-  console.log('result', result)
-  // return
-
-  const cursor = store.cursor
-
-  // const resource = 'files/list_folder' + (cursor ? '/continue')
-
-  let dropboxConfig: any
-
-  if (!cursor) {
-    dropboxConfig = {
-      resource: 'files/list_folder',
-      parameters: {
-          path: '/samples',
-          limit: 2
-      }
-    }
-  } else {
-    dropboxConfig = {
-      resource: 'files/list_folder/continue',
-      parameters: {
-        cursor
-      }
-    }
-  }
-
-  dropbox(dropboxConfig, (err: any, result: any, response: any) => {
-    // console.log('[files/list_folder] error', err)
-
-    if (result.has_more) {
-      store.cursor = result.cursor
-    } else {
-      store.cursor = null
-    }
-    
-    const files = result.entries
-    let i = 0
-
-    for (let file of files) {
-    
-      // console.log('file', file)
-      /**
-        file {
-          '.tag': 'file',
-          name: 'image-1.jpg',
-          path_lower: '/samples/image-1.jpg',
-          path_display: '/samples/image-1.jpg',
-          id: 'id:hiWNdCyIIa0AAAAAAAAAHg',
-          client_modified: '2023-05-05T08:40:07Z',
-          server_modified: '2023-05-05T08:40:07Z',
-          rev: '5faee3c7d24d40df7edef',
-          size: 187741,
-          is_downloadable: true,
-          content_hash: 'efba51e9db838068f3c59fe7cc77a8f726d93bfdfcfb492df5518756a526c7cc'
-        }
-      */
-
-      const imagePath = path.join('images/', file.name)
-
-      dropbox({
-        resource: 'files/download',
-        parameters: {
-            path: file.id,
-        },
-        
-      }, (err: any, result: any, response: any) => {
-        // console.log('backend sync error', err)
-        
-          console.log('file downloaded')
-          // console.log('file downloaded result', result)
-          // console.log('file downloaded response', response)
-
-          i++
-          if (i === files.length && store.cursor) {
-            // redirect to get-images with cursor
-            console.log('here we need to redirect to get-images with cursor', store.cursor)
-            res.redirect('/sync-images')
-          } else {
-            // everything is downloaded
-          }
-      })
-      .pipe(fs.createWriteStream(imagePath, { flags: 'a+' }))
-
-      // console.log('after download ?')
-    }
-  })
 }
 
 router.get('/sync-images', authMiddleware, syncImages)
--- src/service/image.ts
+++ src/service/image.ts
@@ -1,4 +1,6 @@
+import { Request, Response } from 'express'
 import fs from 'fs'
+import fsPromises from "node:fs/promises"
 import path from 'path'
 import { Dropbox } from 'dropbox'
 import store from '../store'
@@ -7,27 +9,59 @@ async function syncImagesService() {
   try {
     const dbx = new Dropbox({ accessToken: store.accessToken })
 
-    const response: any = await dbx.filesListFolder({ path: '/samples' })
+    const response: any = await dbx.filesListFolder({ path: '/images' })
 
     const files = response.result.entries
 
     for (let file of files) {
       const data = await dbx.filesDownload({ path: file.id })
 
-      fs.writeFile(path.join('images', data.result.name), (<any> data).result.fileBinary, { encoding: 'binary' }, (err) => {
-        // if (err) { throw err; }
-        console.log(`File: ${data.result.name} saved.`)
+      const imagePath = path.join('images', data.result.name)
+
+      fs.access(imagePath, function (error) {
+        if (error) {
+          fs.writeFile(
+            path.join('images', data.result.name), 
+            (<any> data).result.fileBinary, 
+            { encoding: 'binary' },
+            (err) => {
+              if (err) { 
+                // console.log(`File: ${data.result.name} exists.`)
+              }
+              console.log(`File: ${data.result.name} saved.`)
+            }
+          )
+        } else {
+          console.log(`File: ${data.result.name} exists.`)
+        }
       })
     }
-
-    // const cursor = store.cursor
-
   } catch(err) {
     console.log(err)
   }
 
 }
 
+async function resetImagesService (_req: Request, res: Response) {
+  store.accessToken = null
+  store.refreshToken = null
+
+  const directory = "images"
+
+  try {
+    await fsPromises.access(directory);
+    
+    for (const file of await fsPromises.readdir(directory)) {
+      await fsPromises.unlink(path.join(directory, file))
+    }
+  } catch {
+    console.error('Folder does not exist')
+  } finally {
+    return res.redirect('/')
+  }
+}
+
 export {
-  syncImagesService
+  syncImagesService,
+  resetImagesService
 }
